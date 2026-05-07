24683bb1fcf6c91bd521a13032a162434a190f00
WEHBEMUMEN <wehbi236@gmail.com>
2026-05-07 15:01:19 +0200

    Fix: Restore OfflineAudit initialization in Phase 4.1a OfflineLab

--- app/simulations/iga-sim-4.1a/src/core/app-core.js
+++ app/simulations/iga-sim-4.1a/src/core/app-core.js
@@ -14,6 +14,7 @@ class OfflineLab {
         
         this.snapshots = []; 
         this.isTraining = false;
+        this.audit = new window.OfflineAudit(this);
         
         this.init();
     }
